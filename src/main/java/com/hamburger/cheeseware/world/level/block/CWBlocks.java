2090b42cc5644ace407fdd1a6801c36ca2606cab
DrEvery <omeguswatchtower@gmail.com>
2025-08-25 18:16:18 -0700

    Commit 9: 08/25/2025 - Added Cheddar Cauldron - Added Curdling Functionality (HOPEFULLY TEMPORARY SYSTEM) - Changed Cheddar Wheel textures - Changed Milk Cauldron contents texture!

--- src/main/java/com/hamburger/cheeseware/world/level/block/CWBlocks.java
+++ src/main/java/com/hamburger/cheeseware/world/level/block/CWBlocks.java
@@ -5,6 +5,7 @@ import net.minecraft.core.cauldron.CauldronInteraction;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.block.AbstractCauldronBlock;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.MapColor;
@@ -15,23 +16,30 @@ import net.neoforged.neoforge.registries.DeferredRegister;
 public class CWBlocks {
     public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(Cheeseware.MODID);
 
-    public static final DeferredBlock<MilkCauldronBlock> MILK_CAULDRON = BLOCKS.registerBlock(
+    public static final DeferredBlock<AbstractCauldronBlock> MILK_CAULDRON = BLOCKS.registerBlock(
             "milk_cauldron",
-            properties -> new MilkCauldronBlock(BlockBehaviour.Properties.of().mapColor(MapColor.STONE).strength(2.0F).requiresCorrectToolForDrops().noOcclusion()
-                    .setId(ResourceKey.create(Registries.BLOCK, ResourceLocation.parse(Cheeseware.MODID + ":milk_cauldron")))
+            properties -> new MilkCauldron(BlockBehaviour.Properties.of().mapColor(MapColor.STONE).strength(2.0F).requiresCorrectToolForDrops().noOcclusion()
+                    .setId(ResourceKey.create(Registries.BLOCK, ResourceLocation.parse(Cheeseware.MODID + ":milk_cauldron"))).randomTicks()
+                    , CauldronInteraction.EMPTY)
+    );
+
+    public static final DeferredBlock<AbstractCauldronBlock> CHEDDAR_CAULDRON = BLOCKS.registerBlock(
+            "cheddar_cauldron",
+            properties -> new CheddarCauldron(BlockBehaviour.Properties.of().mapColor(MapColor.STONE).strength(2.0F).requiresCorrectToolForDrops().noOcclusion()
+                    .setId(ResourceKey.create(Registries.BLOCK, ResourceLocation.parse(Cheeseware.MODID + ":cheddar_cauldron")))
                     , CauldronInteraction.EMPTY)
     );
 
     //Cheese Wheels
     public static final DeferredBlock<CheddarWheel> CHEDDAR_WHEEL_BLOCK = BLOCKS.registerBlock(
             "cheddar_wheel",
-            properties -> new CheddarWheel(BlockBehaviour.Properties.of().noOcclusion().sound(SoundType.HONEY_BLOCK)
+            properties -> new CheddarWheel(BlockBehaviour.Properties.of().strength(0.5F).noOcclusion().sound(SoundType.HONEY_BLOCK)
                     .setId(ResourceKey.create(Registries.BLOCK, ResourceLocation.parse(Cheeseware.MODID + ":cheddar_wheel"))))
     );
 
     public static final DeferredBlock<SwissWheel> SWISS_WHEEL_BLOCK = BLOCKS.registerBlock(
             "swiss_wheel",
-            properties -> new SwissWheel(BlockBehaviour.Properties.of().noOcclusion().sound(SoundType.HONEY_BLOCK)
+            properties -> new SwissWheel(BlockBehaviour.Properties.of().strength(0.5F).noOcclusion().sound(SoundType.HONEY_BLOCK)
                     .setId(ResourceKey.create(Registries.BLOCK, ResourceLocation.parse(Cheeseware.MODID + ":swiss_wheel"))))
     );
 
---
+++ src/main/java/com/hamburger/cheeseware/world/level/block/CheddarCauldron.java
@@ -0,0 +1,32 @@
+package com.hamburger.cheeseware.world.level.block;
+
+import com.mojang.serialization.MapCodec;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.cauldron.CauldronInteraction;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.block.AbstractCauldronBlock;
+import net.minecraft.world.level.block.state.BlockState;
+
+public class CheddarCauldron extends AbstractCauldronBlock {
+    public CheddarCauldron(Properties properties, CauldronInteraction.InteractionMap interactions) {
+        super(properties, interactions);
+    }
+
+    @Override
+    public ItemStack getCloneItemStack(LevelReader level, BlockPos pos, BlockState state, boolean includeData, Player player) {
+        return new ItemStack(Items.CAULDRON);
+    }
+
+    @Override
+    protected MapCodec<? extends AbstractCauldronBlock> codec() {
+        return null;
+    }
+
+    @Override
+    public boolean isFull(BlockState blockState) {
+        return false;
+    }
+}
